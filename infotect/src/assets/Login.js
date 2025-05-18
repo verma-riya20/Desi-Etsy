bddd4dfcc0f30843217932b26b7460a5d2f8afff
Vishal Ramesh <vishalramesh@Vishals-MacBook-Air.local>
2025-05-18 16:13:38 +0530

    assets

---
+++ infotect/src/assets/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Login =()=>{
+    return(
+        <div>
+            <label>Name:
+                <input type="texts"/>
+
+            </label>
+        </div>
+    )
+}
+export default Login;
---
+++ infotect/src/assets/Signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Signup=()=>{
+    return(
+        <div>
+           <p>signup</p> 
+        </div>
+    )
+}
+export default Signup;

5b39e356c9937428afc2475210a61e750167f9b5
verma-riya20 <riyaverma3731@gmail.com>
2025-05-19 15:57:43 +0530

    home page corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Signup from "./components/Signup.jsx";
-import Login from "./components/Login.jsx";
+import './index.css'; // This should match your CSS file name
+
 import Home from "./components/Home.jsx";
-import Nav from "./components/Nav.jsx";
 
 const App=()=>{
     return(
         <div>
             <BrowserRouter>
-            <Nav/>
+            
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
-                <Route path="/Signup" element={<Signup/>}></Route>
-                <Route path="/Login" element={<Login/>}></Route>
-
+               
             </Routes>
             </BrowserRouter>
         </div>
